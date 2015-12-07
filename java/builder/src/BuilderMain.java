71f0e0e87581fa37cc4ece424dd918b2517bf997
rain <brother.rain.1024@gmail.com>
2015-12-07 15:33:56 +0700

    initail commit

---
+++ java/builder/src/BuilderMain.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rain on 12/7/2015.
+ */
+public class BuilderMain {
+}
---
+++ java/decorator/src/DecoratorMain.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rain on 12/7/2015.
+ */
+public class DecoratorMain {
+    public static void main(String[] args) {
+        System.out.println("hihi");
+    }
+}
